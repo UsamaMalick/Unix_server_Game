e3be04e83ae3c75e7cb0c266d33a6fb1526bea53
UsamaMalick <you@example.com>
2020-05-10 06:21:12 +0500

    User and password Validation complete. Administrator permission function Written and tested successfully.

--- administrator.c
+++ administrator.c
@@ -9,6 +9,25 @@
 
 
 
+bool allow(char * username){
+    
+    char allow[1];
+    
+    printf("\nDo you want to allow user to enter in the game room. \n");
+    printf("User name is : ");
+    printf("%s\n" , username);
+    printf("Please Select an option [y/n] : ");
+    gets(allow);
+    
+    if (allow[0] == 'y') {
+    	printf("User allowed to enter the Game Room. \n");
+    	return true;
+    }
+    printf("User is not allowed to Enter the Game Room. \n");
+    return false;
+
+}
+
 bool isUser(char * UserIncoming){
 
     FILE *fp;
@@ -26,21 +45,13 @@ bool isUser(char * UserIncoming){
         
         int string_size = strlen(str);
         int user_size = strlen(UserIncoming);
-        
-        printf("String Lengths : ");
-        printf("%d" , string_size);
-        printf("%d\n" , user_size);
-        printf("Strings : " );
-        printf("%s", str);    
-        printf(" : " );
-        printf("%s\n", UserIncoming);
+ 
         
         if(string_size == user_size) {
-        	printf("\nSize equal checking values. \n");
+        	
         	is_user = false;       	
         	for(int i=0; i<user_size; i++){
-        	     printf("%c", UserIncoming[i]);
-        	     printf("%c", str[i]);
+        	    
         	     if(UserIncoming[i] == str[i]) {
         	     	is_user = true;
         	     }
@@ -51,9 +62,7 @@ bool isUser(char * UserIncoming){
         	
         	}
         	if(is_user == true){
-        	     printf("IN TRUE");
-        	     printf("%s", str);
-        	     printf("%s\n", UserIncoming);
+        	     
         	     fclose(fp);
         	     return is_user;	
         	}
@@ -71,11 +80,13 @@ int main(void)
         int socket_desc, client_sock, client_size; 
         struct sockaddr_in server_addr, client_addr;         
 	//SERVER ADDR will have all the server address
-        char server_message[2000], client_message[2000];                 
+        char server_message[2000], client_message[2000];
+        char username[100];       
 	// Sending values from the server and receive 
 	//from the server we need this
         //Cleaning the Buffers
         
+        memset(username,'\0',sizeof(username));
         memset(server_message,'\0',sizeof(server_message));
         memset(client_message,'\0',sizeof(client_message));     
 	// Set all bits of the padding field//        
@@ -105,11 +116,11 @@ int main(void)
 	// BINDING FUNCTION
         
         if(bind(socket_desc, (struct sockaddr*)&
-	server_addr, 	sizeof(server_addr))<0)    
+	server_addr, 	sizeof(server_addr))<0) { 
 	// Bind the address struct to the socket.  /
 	//bind() passes file descriptor, the address structure,
 	//and the length of the address structure
-        {
+        
                 printf("Bind Failed. Error!!!!!\n");
                 return -1;
         }        
@@ -119,13 +130,13 @@ int main(void)
         //Put the socket into Listening State
 
 	 
-        if(listen(socket_desc, 1) < 0)                              
+        if(listen(socket_desc, 1) < 0) {                            
 	 // This listen() call tells the socket to listen 
 	 // to the incoming connections.
 	 // The listen() function places all incoming 
 	 // connection into a "backlog queue" until accept()
 	 // call accepts the connection.
-         {
+         
                 printf("Listening Failed. Error!!!!!\n");
                 return -1;
          }
@@ -134,38 +145,66 @@ int main(void)
         //Accept the incoming Connections
         
         client_size = sizeof(client_addr);
+        
+        while(1){	
 
-	while(1){	
+	printf("Listening for Incoming Connections.....\n");
+         	memset(client_message,'\0',sizeof(client_message)); 
+         	memset(username,'\0',sizeof(username));
+         	memset(server_message,'\0',sizeof(server_message));    
 	
-	    printf("Listening for Incoming Connections.....\n");
-	    memset(client_message,'\0',sizeof(client_message));     
-		
-	    client_sock = accept(socket_desc, 
-	    (struct sockaddr*)&client_addr, &client_size);
-		
-		if (client_sock < 0)
-		{
-		        printf("Accept Failed. Error!!!!!!\n");
-		        return -1;
-		}
-		
-		printf("Client Connected with IP: %s and Port No: %i\n", 					inet_ntoa(client_addr.sin_addr),ntohs(client_addr.sin_port));
-	       //inet_ntoa() function converts the Internet  			   
-		//host address in, given in network byte order, 
-		//to a string in IPv4 dotted-decimal notation
+	client_sock = accept(socket_desc, 
+	(struct sockaddr*)&client_addr, &client_size);
 
-	    if (recv(client_sock, client_message, sizeof(client_message),0) < 0)
-		{
-		        printf("Receive Failed. Error!!!!!\n");
-		        return -1;
-		}
-		
-		//printf("Client Message: %s\n",client_message);
-		if ( isUser(client_message) == true) {
-			printf("Client Validated : \n");
-		}
-		else printf("\nClient rejected : \n");
+	if (client_sock < 0){
+		printf("Accept Failed. Error!!!!!!\n");
+		return -1;
+	}
+	printf("Client Connected with IP: %s and Port No: %i\n", 		
+	inet_ntoa(client_addr.sin_addr),ntohs(client_addr.sin_port));
+	//inet_ntoa() function converts the Internet  			   
+	//host address in, given in network byte order, 
+	//to a string in IPv4 dotted-decimal notation
+
+	if (recv(client_sock, client_message, sizeof(client_message),0) < 0){
+		printf("Receive Failed. Error!!!!!\n");
+		return -1;
 	}
 
+	//printf("Client Message: %s\n",client_message);
+	if ( isUser(client_message) == true) {
+		
+		for (int i=0; client_message[i] != '@' ; i++){
+			username[i] = client_message[i];	
+		}
+		printf("Player Validated : \n");
+		allow(username);
+	}
+	else printf("\nClient rejected : \n");
+        }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         
